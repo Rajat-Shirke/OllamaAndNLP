8e53c0ba6921a1583837d2328379b5148fdeabc9
Rajat Shirke <rajatshirke94@gmail.com>
2025-04-21 10:48:38 +0530

    Add files via upload

---
+++ src/main/java/com/learning/OllamaAndNLP/OllamaAndNlpApplication.java
@@ -0,0 +1,13 @@
+package com.learning.OllamaAndNLP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaAndNlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaAndNlpApplication.class, args);
+	}
+
+}
